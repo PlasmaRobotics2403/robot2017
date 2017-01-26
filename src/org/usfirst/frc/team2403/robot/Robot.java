387ce6f44bff03030139595adac759629f710c4a
nardavin <nardavin@gmail.com>
2017-01-25 18:42:09 -0700

    Built code skeleton, added PlasmaJoystick classes

---
+++ src/org/usfirst/frc/team2403/robot/Robot.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team2403.robot;
+
+import edu.wpi.first.wpilibj.*;
+
+public class Robot extends IterativeRobot {
+
+
+	@Override
+	public void robotInit() {
+
+	}
+
+	@Override
+	public void autonomousInit() {
+
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+
+	}
+	
+	@Override
+	public void teleopInit(){
+		
+	}
+	
+	@Override
+	public void teleopPeriodic() {
+		
+	}
+	
+	@Override
+	public void testInit(){
+		
+	}
+
+	@Override
+	public void testPeriodic() {
+		
+	}
+}
+
